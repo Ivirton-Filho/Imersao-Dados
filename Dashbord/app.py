09c248a68991b19fcc02add383a8fb8da795f9b7
ivirton <ivirtonfilho@gmail.com>
2026-02-08 16:34:51 -0300

    Main Content

--- Dashbord/app.py
+++ Dashbord/app.py
@@ -38,3 +38,26 @@ df_filtrado = df [
     (df["employment_type"].isin(contratos_selecionados)) &
     (df["company_size"].isin(tamanhos_selecionados))
 ]
+
+# Conteúdo Principal
+st.title("Dashbord de Análise de Salários na Área de Dados")
+st.markdown("Explore os dados salariais na área de dados nos últimos anos.")
+
+#Metricas Principais (KPIs)
+st.subheader("Métricas gerais (Sálario anual em USD)")
+
+if not df_filtrado.empty:
+    salario_medio = df_filtrado["salary_in_usd"].mean()
+    salario_maximo = df_filtrado["salary_in_usd"].max()
+    total_registros = df_filtrado.shape[0]
+    cargo_mais_frequente = df_filtrado["job_title"].mode()[0]
+else:
+    salario_medio, salario_mediano, salario_maximo, total_registros, cargo_mais_comum = 0, 0, 0, 0, ""
+
+col1, col2, col3, col4 = st.columns(4)
+col1.metric("Salário médio", f"${salario_medio:,.0f}")
+col2.metric("Salário máximo", f"${salario_maximo:,.0f}")
+col3.metric("Total de registros", f"${total_registros:,.0f}")
+col4.metric("Cargo mais frequente", cargo_mais_frequente)
+
+st.markdown("---")
